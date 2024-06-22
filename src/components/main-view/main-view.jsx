7f45ebf67c9531181a8ba76e6758cd16fa6c538e
taylorhech <114107307+taylorhech@users.noreply.github.com>
2024-06-21 18:56:34 -0700

    Added PropTypes & API url
    Added PropTypes to MovieCard
    Fetch data from API in MainView
    Updated MovieView to properly display Genre & Director names

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,34 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            Title: "Fight Club",
-            Director: "David Fincher",
-            Genre: "Drama",
-            Description: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
-            Image: "https://image.tmdb.org/t/p/original/A86dg5r6tdUVvQBeOGhvgTXGoQi.jpg"
-        },
-        {
-            id: 2,
-            Title: "Inception",
-            Director: "Christopher Nolan",
-            Genre: "Action",
-            Description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.",
-            Image: "https://image.tmdb.org/t/p/original/gWyowg9gwFBBj9YPbLd67dDK7GF.jpg"
-        },
-        {
-            id: 3,
-            Title: "The Dark Knight",
-            Director: "Christopher Nolan",
-            Genre: "Action",
-            Description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-            Image: "https://image.tmdb.org/t/p/original/3otEafr8bn78vqQQTg9omTyE6Il.jpg"
-        },
-    ]);
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch("https://themovieflix-ab7619e62e53.herokuapp.com/movies")
+        .then((response) => response.json())
+        .then((data) => {
+            const moviesFromApi = data.map((movie) => {
+                return {
+                    id: movie._id,
+                    Title: movie.Title,
+                    Genre: movie.Genre,
+                    Description: movie.Description,
+                    Director: movie.Director,
+                    Image: movie.ImageUrl
+                };
+            });
+            setMovies(moviesFromApi);
+        });
+    });
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
@@ -8,4 +10,15 @@ export const MovieCard = ({ movie, onMovieClick }) => {
             {movie.Title}
         </div>
     );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string,
+    Genre: PropTypes.string,
+    Director: PropTypes.string,
+    Image: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
